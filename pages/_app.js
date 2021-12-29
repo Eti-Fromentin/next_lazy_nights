a62625da18f15db451029be389773c78038f4b2a
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-12-30 00:27:09 +0100

    vercel deployement

--- pages/_app.js
+++ pages/_app.js
@@ -48,7 +48,7 @@ function MyApp({ Component, pageProps }) {
             </CurrentFinalChoicesContextProvider>
           </CurrentAllFavoritesContextProvider>
         </CurrentUserNameContextProvider>
-      </MobileView> 
+      </MobileView>
     </>
   );
 }
